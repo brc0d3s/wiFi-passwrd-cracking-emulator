d0a230333248eb4561a3f026e5c8492014ac0716
brc0d3s <107584987+brc0d3s@users.noreply.github.com>
2022-06-17 07:36:05 +0300

    Create wifi.py

---
+++ wifi.py
@@ -0,0 +1,244 @@
+//WIFI HACKER BY BRC0D3S 
+
+
+print("  ~~~~~~~~~~~~~~~~~~~WIFI HACKER~~~~~~~~~~~~~~~~~~~~~    ")
+
+if input('TYPE "start" TO START THE HACKING WIFI PROCESS .\n> ') == 'start': 
+ import time
+time.sleep(3)
+print("CONECTED TO THE SERVER ")
+time.sleep(2)
+print("PROCESS STARTED! ")
+print(".     ")
+time.sleep(1)
+name = input("WIFI NAME ? ->")
+print(".            ")
+
+import time 
+time.sleep(1)
+print("TESTING ALL PIN ON",name)
+time.sleep(1)
+print("PIN 1383838 FAILED! ")
+time.sleep(0.5)
+print("PIN 729293 FAILED! ")
+time.sleep(0.5)
+print("PIN 74397493 FAILED! ")
+time.sleep(0.5)
+print("PIN 738349 FAILED! ")
+time.sleep(0.5)
+print("PIN 37383 FAILED! ")
+time.sleep(0.5)
+print("PIN 8320293 FAILED! ")
+time.sleep(0.5)
+print("PIN 833920 FAILED! ")
+time.sleep(0.5)
+print("PIN 38383 FAILED! ")
+time.sleep(0.5)
+print("PIN 38383 FAILED! ")
+time.sleep(0.5)
+print("PIN 483939 FAILED! ")
+time.sleep(0.5)
+print("PIN 4939339 FAILED! ")
+time.sleep(0.5)
+print("PIN 38339 FAILED! ")
+time.sleep(0.5)
+print("PIN 4933930 FAILED! ")
+time.sleep(0.5)
+print("PIN 49493939 FAILED! ")
+time.sleep(0.5)
+print("PIN 493933493 FAILED! ")
+time.sleep(0.5)
+print("PIN 4943903 FAILED! ")
+time.sleep(0.5)
+print("PIN 484394949 FAILED! ")
+time.sleep(0.5)
+print("PIN 392837 FAILED! ")
+time.sleep(0.5)
+print("PIN 20284383 FAILED! ")
+time.sleep(0.5)
+print("PIN 7348383 FAILED! ")
+time.sleep(0.5)
+print("PIN 38383899 FAILED! ")
+time.sleep(0.5)
+print("PIN 7819479 FAILED! ")
+time.sleep(0.5)
+print("PIN 28395040 FAILED! ")
+time.sleep(0.5)
+print("PIN 485740330 FAILED! ")
+time.sleep(0.5)
+print("PIN 37393959 FAILED! ")
+time.sleep(0.5)
+print("PIN 74384848 FAILED! ")
+time.sleep(0.5)
+print("PIN 17484930 FAILED! ")
+time.sleep(0.5)
+print("PIN 1234695 FAILED! ")
+time.sleep(0.5)
+print("PIN 8393929348 FAILED! ")
+time.sleep(0.5)
+print("PIN 382930505 FAILED! ")
+time.sleep(0.5)
+print("PIN 3833895 FAILED! ")
+time.sleep(0.5)
+print("PIN 494949 FAILED! ")
+time.sleep(0.5)
+print("PIN 484959 FAILED! ")
+time.sleep(0.5)
+print("PIN 4939506 FAILED! ")
+time.sleep(0.5)
+print("PIN 3849590 FAILED! ")
+time.sleep(0.5)
+print("PIN 48393949 FAILED! ")
+time.sleep(0.5)
+print("PIN 38485060 FAILED! ")
+time.sleep(0.5)
+print("PIN 39302058 FAILED! ")
+time.sleep(0.5)
+print("PIN 3830595o FAILED! ")
+time.sleep(0.5)
+print("PIN 830350648 FAILED! ")
+time.sleep(0.5)
+print("PIN 48539495 FAILED! ")
+time.sleep(0.5)
+print("PIN 48496063 FAILED! ")
+time.sleep(0.5)
+print("PIN 123456789 FAILED! ")
+time.sleep(0.5)
+print("PIN 0123456789 FAILED! ")
+time.sleep(0.5)
+print("PIN 0 WORK! ")
+time.sleep(2)
+print("                                                     ")
+print("ENCOED PASSWORDS FOUND! ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("                                                       ")
+
+
+
+import time
+time.sleep(1)
+print("ENCODING PASSWORD..... ")
+time.sleep(0.3)
+print("%1")
+time.sleep(0.3)
+print("%15")
+time.sleep(0.3)
+print("%20")
+time.sleep(0.3)
+print("%25")
+time.sleep(0.3)
+print("%30")
+time.sleep(0.3)
+print("%35")
+time.sleep(0.5)
+print("%47")
+time.sleep(0.4)
+print("%51")
+time.sleep(0.2)
+print("%60")
+time.sleep(0.9)
+print("%63")
+time.sleep(0.6)
+print("%78")
+time.sleep(0.3)
+print("%80")
+time.sleep(0.3)
+print("%85")
+time.sleep(0.1)
+print("%90")
+time.sleep(0.8)
+print("%97")
+time.sleep(0.7)
+print("%99")
+time.sleep(1)
+print("%100")
+
+print("PASSWORD FOUND")
+
+
+
+
+time.sleep(2)
+import string
+
+from random import *
+
+
+
+letters = string.ascii_letters 
+
+digits = string.digits 
+
+
+chars = letters + digits
+
+
+
+min_len = 8
+
+max_len = 16
+
+password = "".join(choice(chars) for x in range(randint(min_len, max_len)))
+print ("=========================================")
+
+print("PASSWORD IS",password)
+print ("=========================================")
+
+time.sleep(1)
+
+print ("=========================================")
+
+import socket
+hostn = socket.gethostname()
+IPAD = socket.gethostbyname(hostn)
+print ("ADRESSE IP IS",IPAD)
+
+print ("=========================================")
+ 
+ 
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print ("=========================================")
+print("plz put me a star")
+print ("=========================================")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+print("plz put me a star")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
